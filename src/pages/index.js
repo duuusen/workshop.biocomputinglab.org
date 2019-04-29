8772fdaa058dd42764191023224cf997b5fd0ce7
duuusen <halloduy@gmail.com>
2019-04-29 11:13:32 +0200

    added bdcblog link

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`biological computing lab`, `bcl`, `interaction design`]} />
+  <h1 className={'bdcLink'}><a href="https://bdc.iad17.wtf">&#8674; Biodesign Challenge Blog</a></h1>
   </Layout>
 )
 
