af9a159aae69e7f3f111f3aaf77eba5a857af857
duuusen <halloduy@gmail.com>
2019-05-07 13:13:36 +0200

    initial duymit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`biological computing lab`, `bcl`, `interaction design`]} />
-  <h1 className={'bdcLink'}><a href="https://bdc.iad17.wtf">&#8674; Biodesign Challenge Blog</a></h1>
+  <section>
+  <h1>Fermentation Workshop</h1>
+  <br/>
+  <p>
+ The Bio Design Challenge Team from the 4th Semester Interaction Design Bachelor is excited to invite you to the first Ad-Hoc Fermentation Workshop at Toni Areal. 
+Fermentation is a metabolic process that has in most cultures been used for millennia to make foodstuffs less perishable or simply transform them. One prominent example is Kimchi, a staple in Korean cuisine.
+The format of our workshop will be facilitating the exchange of knowledge about a variety of fermentation traditions, procedures and recipes in a casual environment. Everyone is invited to bring fresh vegetables (carrots, scallions, garlic, ginger and asparagus…) and other needed ingredients. Necessary are a clean, sealable glass container in the preferred size, sea salt, small kitchen knife and chilli powder. The Chinese cabbage will be prepared and provided by us. Join in the fun! 
+  </p>
+  <br/>
+  <a className={'bdcLink'} href="https://bdc.iad17.wtf">&#8674; Biodesign Challenge Blog</a>
+  </section>
   </Layout>
 )
 
